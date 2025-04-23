d73ca391361a1ad8a86b453aa070f21c761346cf
rafafrd <bolekao03@gmail.com>
2025-04-23 09:25:20 -0300

    Add verificação de numero valido

--- 2.js
+++ 2.js
@@ -2,12 +2,17 @@ numeroCerto = Math.ceil(Math.random() * 10);
 let tentativas = 3;
 for (let t = 1; t <= tentativas; t++) {
   let chute = parseInt(prompt(`Tentativa ${t}: Adivinhe o número (1 a 10):`));
-  if (chute === numeroCerto) {
-    alert(`Você acertou! O número certo era ${numeroCerto}.`);
-    break;
-  } else if (t === tentativas) {
-    alert(`Suas chances acabaram! O número certo era ${numeroCerto}.`);
+  if (!isNaN(chute)) {
+    if (chute === numeroCerto) {
+      alert(`Você acertou! O número certo era ${numeroCerto}.`);
+      break;
+    } else if (t === tentativas) {
+      alert(`Suas chances acabaram! O número certo era ${numeroCerto}.`);
+    } else {
+      alert("Tente outro novamente!");
+    }
   } else {
-    alert("Tente outro novamente!");
+    alert("Insira um numero valido!");
+    break;
   }
 }
